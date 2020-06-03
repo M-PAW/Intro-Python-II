33fd4ce23039a89eebf1a343fad634dfffdd2d09
Michael Walter <michael@MacBook-Pro.local>
2020-06-03 11:47:51 -0600

    Building out room, player, and adv. room and player complete, starting on adv.

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class player():
+    def __init__(self, player_name, current_room):
+        self.player_name = player_name
+        self.current_room = current_room
+
+    def move(self, direction):
+        if getattr(self.current_room, f"{direction}_to") is not None: self.current_room = getattr(self.current_room,f"{direction_to")
+        else:
+            print("They way is impassable!")
